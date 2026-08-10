2bf4ef69fb2fa166d14dc17bb0da48db7e3f4cad
Shriram <shriram.vibhute08@gmail.com>
2026-08-10 07:10:02 +0000

    Added code demonstrating method overriding and accessing base class overrided method

---
+++ Object Oriented Programming/Class and Object/Method Overriding.cpp
@@ -0,0 +1,38 @@
+/*
+    The function name, return type, number of arguments, type of arguments if all of these are same then this is called method overriging
+*/
+
+# include <bits/stdc++.h>
+using namespace std;
+
+class First {
+    public:
+        void print(string a) {
+            cout << "base → " << a << endl;
+        };
+};
+
+class Second : public First {
+    public:
+        void print(string a) { 
+            cout << "derived → " << a << endl; 
+        }; // method overriding → method inherited from based class
+    
+        void print(int a) {
+            cout << a << endl;
+        }; // method overloading → same class have same method but different data typed parameters 
+};
+
+int main() {
+    Second obj;
+
+    /* calling base class method using derived class object */
+    obj.First::print("calling base class methods");
+
+    /* derived class method */
+    obj.print("calling derived class method");
+    obj.Second::print("calling derived class method");
+    obj.print(10);
+    
+    return 0;
+};
